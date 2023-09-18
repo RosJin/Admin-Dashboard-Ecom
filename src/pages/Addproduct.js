57975aabaa00685979a951987fc04a03374b4ba0
RosJin <huyhoang.hn462@gmail.com>
2023-09-19 02:53:30 +0700

    create files

---
+++ src/pages/Addproduct.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Addproduct = () => {
+    return (
+        <div>
+            <h3 className="mb-4">Add Product</h3>
+            <div>
+                <form action="">
+                    <button
+                        className="btn btn-success border-0 rounded-3 my-5"
+                        type="submit">
+                        Add Product
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Addproduct;
