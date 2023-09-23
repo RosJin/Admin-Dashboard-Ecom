8eb08d1184b2b5bb432b389a1c599d85ce75cd70
RosJin <huyhoang.hn462@gmail.com>
2023-09-23 17:43:02 +0700

    add product

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,12 @@ import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/customers/customerSlice";
 import productReducer from "../features/product/productSlice";
 import brandReducer from "../features/brand/brandSlice";
-import pCategoryReducer from '../features/pcategory/pcategorySlice';
-import bCategoryReducer from '../features/bcategory/bcategorySlice';
-import blogReducer from '../features/blogs/blogSlice';
-import colorReducer from '../features/color/colorSlice';
-import enquiryReducer from '../features/enquiry/enquirySlice'
-
+import pCategoryReducer from "../features/pcategory/pcategorySlice";
+import bCategoryReducer from "../features/bcategory/bcategorySlice";
+import blogReducer from "../features/blogs/blogSlice";
+import colorReducer from "../features/color/colorSlice";
+import enquiryReducer from "../features/enquiry/enquirySlice";
+import uploadReducer from "../features/upload/uploadSlice";
 
 export const store = configureStore({
     reducer: {
@@ -21,5 +21,6 @@ export const store = configureStore({
         blog: blogReducer,
         color: colorReducer,
         enquiry: enquiryReducer,
+        upload: uploadReducer,
     },
 });
