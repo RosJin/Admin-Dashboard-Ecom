d50af93f0fa2aae1693f7d0fdca32cfcfdcef517
RosJin <huyhoang.hn462@gmail.com>
2023-09-30 21:38:48 +0700

    update

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { base_url } from "../../utils/base_url";
 import { config } from "../../utils/axiosconfig";
 
-
 const login = async (user) => {
     const response = await axios.post(`${base_url}user/admin-login`, user);
     if (response.data) {
@@ -17,8 +16,17 @@ const getOrders = async () => {
     return response.data;
 };
 
+const getOrder = async (id) => {
+    const response = await axios.post(
+        `${base_url}user/getorderbyuser/${id}`,"", config,
+    );
+
+    return response.data;
+};
+
 const authService = {
     login,
     getOrders,
+    getOrder,
 };
 export default authService;
--- src/pages/ViewOrder.js
+++ src/pages/ViewOrder.js
@@ -3,8 +3,8 @@ import { Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { BiEdit } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import { getOrders } from "../features/auth/authSlice";
+import { Link, useLocation } from "react-router-dom";
+import { getOrderByUser, getOrders } from "../features/auth/authSlice";
 
 const columns = [
     {
@@ -12,12 +12,20 @@ const columns = [
         dataIndex: "key",
     },
     {
-        title: "Name",
+        title: "Product Name",
         dataIndex: "name",
     },
     {
-        title: "Product",
-        dataIndex: "product",
+        title: "Brand",
+        dataIndex: "brand",
+    },
+    {
+        title: "Count",
+        dataIndex: "count",
+    },
+    {
+        title: "Color",
+        dataIndex: "color",
     },
     {
         title: "Amount",
@@ -33,24 +41,24 @@ const columns = [
     },
 ];
 
-const Orders = () => {
+const ViewOrder = () => {
+    const location = useLocation()
+    const userId = location.pathname.split("/")[3]
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(getOrders());
+        dispatch(getOrderByUser(userId));
     }, []);
-    const orderState = useSelector((state) => state.auth.orders);
+    const orderState = useSelector((state) => state.auth.orderbyuser.products);
     const data1 = [];
     for (let i = 0; i < orderState.length; i++) {
         data1.push({
             key: i + 1,
-            name: orderState[i].orderby.firstname,
-            product: (
-                <Link to={`/admin/order/${orderState[i].orderby._id}`}>
-                View Orders
-                </Link>
-            ),
-            amount: orderState[i].paymentIntent.amount,
-            date: new Date(orderState[i].createdAt).toLocaleString(),
+            name: orderState[i].product.title,
+            brand: orderState[i].product.brand,
+            count: orderState[i].count,
+            amount: orderState[i].product.price,
+            color: orderState[i].product.color,
+            date: orderState[i].product.createdAt,
             action: (
                 <>
                     <Link to="/" className="fs-3 text-danger">
@@ -65,7 +73,7 @@ const Orders = () => {
     }
     return (
         <div>
-            <h3 className="mb-4 title">Orders</h3>
+            <h3 className="mb-4 title">View Order</h3>
             <div>
                 <Table columns={columns} dataSource={data1} />
             </div>
@@ -73,4 +81,4 @@ const Orders = () => {
     );
 };
 
-export default Orders;
+export default ViewOrder;
