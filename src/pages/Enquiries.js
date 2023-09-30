72ee22bbef7a95ecfc7e0b3eb3631c9c36cb432f
RosJin <huyhoang.hn462@gmail.com>
2023-09-30 19:14:24 +0700

    update

--- src/pages/Enquiries.js
+++ src/pages/Enquiries.js
@@ -1,9 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
-import { AiFillDelete } from "react-icons/ai";
+import { AiFillDelete, AiOutlineEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { getEnquiries } from "../features/enquiry/enquirySlice";
+import {
+    deleteAEnquiry,
+    getEnquiries,
+    resetState,
+    updateAEnquiry,
+} from "../features/enquiry/enquirySlice";
+import CustomModal from "../components/CustomModal";
 
 const columns = [
     {
@@ -26,7 +32,7 @@ const columns = [
         title: "Status",
         dataIndex: "status",
     },
-   
+
     {
         title: "Action",
         dataIndex: "action",
@@ -35,7 +41,19 @@ const columns = [
 
 const Enquiries = () => {
     const dispatch = useDispatch();
+    const [open, setOpen] = useState(false);
+    const [enqId, setenqId] = useState("");
+    const showModal = (e) => {
+        setOpen(true);
+        setenqId(e);
+    };
+
+    const hideModal = () => {
+        setOpen(false);
+    };
+
     useEffect(() => {
+        dispatch(resetState());
         dispatch(getEnquiries());
     }, []);
     const enqState = useSelector((state) => state.enquiry.enquiries);
@@ -48,26 +66,66 @@ const Enquiries = () => {
             mobile: enqState[i].mobile,
             status: (
                 <>
-                    <select name="" className="form-control form-select" id="">
-                        <option value="">Set status</option>
+                    <select
+                        name=""
+                        defaultValue={
+                            enqState[i].status
+                                ? enqState[i].status
+                                : "Submitted"
+                        }
+                        className="form-control form-select"
+                        id=""
+                        onChange={(e) =>
+                            setEnquiryStatus(e.target.value, enqState[i]._id)
+                        }>
+                        <option value="Submitted">Submitted</option>
+                        <option value="Contacted">Contacted</option>
+                        <option value="In Progress">In Progress</option>
+                        <option value="Resolved">Resolved</option>
                     </select>
                 </>
             ),
             action: (
                 <>
-                    <Link to="/" className="ms-3 fs-3 text-danger">
-                        <AiFillDelete />
+                    <Link
+                        to={`/admin/enquiries/${enqState[i]._id}`}
+                        className="ms-3 fs-3 text-danger">
+                        <AiOutlineEye />
                     </Link>
+                    <button
+                        className="ms-3 fs-3 text-danger bg-transparent border-0"
+                        onClick={() => showModal(enqState[i]._id)}>
+                        <AiFillDelete />
+                    </button>
                 </>
             ),
         });
     }
+    const setEnquiryStatus = (e, i) => {
+        const data = { id: i, enqData: e };
+        dispatch(updateAEnquiry(data));
+    };
+    const deleteEnq = (e) => {
+        dispatch(deleteAEnquiry(e));
+        setOpen(false);
+        setTimeout(() => {
+            dispatch(getEnquiries());
+        }, 100);
+    };
     return (
         <div>
             <h3 className="mb-4 title">Enquiries</h3>
             <div>
                 <Table columns={columns} dataSource={data1} />
             </div>
+            <CustomModal
+                hideModal={hideModal}
+                open={open}
+                performAction={() => {
+                    deleteEnq(enqId);
+                }}
+                title="Are you sure you want to delete this enquiry?"
+            />
         </div>
     );
 };
